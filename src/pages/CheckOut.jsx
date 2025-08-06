0158648324243618ebd28262ad8e8a8cca0ca9a8
Bhavana <Bhavanabharath@gmail.com>
2025-08-06 10:18:12 +0530

    Updated files

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -1,43 +1,6 @@
 export default function Checkout() {
   return (
-    // <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-    //   <div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full">
-    //     <h2 className="text-2xl font-bold mb-6 text-center">Checkout</h2>
-
-    //     <form>
-    //       <div className="mb-3">
-    //         <label className="block mb-1">Full Name</label>
-    //         <input type="text" className="w-full border px-4 py-2 rounded" required />
-    //       </div>
-
-    //       <div className="mb-3">
-    //         <label className="block mb-1">Phone Number</label>
-    //         <input type="Number" className="w-full border px-4 py-2 rounded"  pattern="\d{10}" required />
-    //       </div>
-
-    //       <div className="mb-3">
-    //         <label className="block mb-1">Address</label>
-    //         <textarea className="w-full border px-4 py-2 rounded" rows="3" required></textarea>
-    //       </div>
-
-    //       <div className="mb-3">
-    //         <label className="block mb-1">Payment Method</label>
-    //         <select className="w-full border px-4 py-2 rounded">
-    //           <option>Cash on Delivery</option>
-    //           <option>UPI</option>
-    //           <option>Credit/Debit Card</option>
-    //         </select>
-    //       </div>
-
-    //       <button
-    //         type="submit"
-    //         className="w-full bg-red-400 text-white py-3 rounded-lg text-lg font-semibold hover:bg-red-500 transition"
-    //        onClick={alert("Your order placed Successfully")}>
-    //         Place Order
-    //       </button>
-    //     </form>
-    //   </div>
-    // </div>
+    
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
   <div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full">
     <h2 className="text-2xl font-bold mb-6 text-center">Checkout</h2>
